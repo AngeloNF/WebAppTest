6d66e3b3a974f9a3b3d38db34cbd0ffd7f92b304
AngeloNF <djaanf@gmail.com>
2020-02-26 16:40:51 -0600

    First Test with git

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+include_once 'includes/survey.php'
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="" method="post">
+        <?php
+        $survie = new Survey();
+        $showResults = false;
+
+        if (isset($_POST['lenguaje'])) {
+            $showResults = true;
+            $survie->setOptionSelected($_POST['lenguaje']);
+            $survie->vote();
+        }
+
+
+        ?>
+        <h2>¿Cual es tu lenguaje de programación favorito?</h2>
+        <?php
+        if ($showResults) {
+            $lenguajes = $survie->showResults();
+
+            echo '<h2>' . $survie->getTotalVotes() . ' votos totales </h2>';
+
+            foreach ($lenguajes as $lenguaje) {
+                $porcentaje = $survie->getPercentageVotes($lenguaje['votos']);
+                include 'vistas/vista-resultado.php';
+            }
+            $showResults = false;
+        }else{
+            include 'vistas/vista-votacion.php';
+        }
+
+        ?>
+
+
+    </form>
+</body>
+
+</html>
