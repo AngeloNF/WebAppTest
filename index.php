b6ed4b666a35bc1a83a2d51f92b3f13df8ad5d3f
AngeloNF <djaanf@gmail.com>
2020-02-26 17:49:05 -0600

    Esta es una prueba de git

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include_once 'includes/survey.php'
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="main.css">
-    <title>Document</title>
+    <title>Prueba Web App</title>
 </head>
 
 <body>
